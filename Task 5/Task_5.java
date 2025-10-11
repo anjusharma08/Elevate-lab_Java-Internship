c4dafae9a07ded2a9defd8f65936733852e7a907
anjusharma08 <anjusharma171684@gmail.com>
2025-10-11 23:30:03 +0530

    Add files via upload

---
+++ Task 5/Task_5.java
@@ -0,0 +1,106 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// Account class
+class Account {
+    private String accountHolder;
+    private double balance;
+    private ArrayList<String> transactionHistory;
+
+    // Constructor
+    public Account(String accountHolder, double initialBalance) {
+        this.accountHolder = accountHolder;
+        this.balance = initialBalance;
+        this.transactionHistory = new ArrayList<>();
+        transactionHistory.add("Account created with initial balance: " + initialBalance);
+    }
+
+    // Deposit method
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            transactionHistory.add("Deposited: " + amount + " | Balance: " + balance);
+            System.out.println("Deposited " + amount + ". Current Balance: " + balance);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    // Withdraw method
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            transactionHistory.add("Withdrew: " + amount + " | Balance: " + balance);
+            System.out.println("Withdrew " + amount + ". Current Balance: " + balance);
+        } else {
+            System.out.println("Insufficient balance or invalid amount.");
+        }
+    }
+
+    // Check balance
+    public double getBalance() {
+        return balance;
+    }
+
+    // Show transaction history
+    public void showTransactionHistory() {
+        System.out.println("\nTransaction History for " + accountHolder + ":");
+        for (String transaction : transactionHistory) {
+            System.out.println(transaction);
+        }
+    }
+}
+
+// Main class
+public class Task_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Create account
+        System.out.print("Enter account holder name: ");
+        String name = sc.nextLine();
+        System.out.print("Enter initial balance: ");
+        double initialBalance = sc.nextDouble();
+
+        Account account = new Account(name, initialBalance);
+
+        int choice;
+        do {
+            System.out.println("\n--- Bank Menu ---");
+            System.out.println("1. Deposit");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Check Balance");
+            System.out.println("4. Transaction History");
+            System.out.println("5. Exit");
+            System.out.print("Choose an option: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter deposit amount: ");
+                    double depositAmount = sc.nextDouble();
+                    account.deposit(depositAmount);
+                    break;
+                case 2:
+                    System.out.print("Enter withdraw amount: ");
+                    double withdrawAmount = sc.nextDouble();
+                    account.withdraw(withdrawAmount);
+                    break;
+                case 3:
+                    System.out.println("Current Balance: " + account.getBalance());
+                    break;
+                case 4:
+                    account.showTransactionHistory();
+                    break;
+                case 5:
+                    System.out.println("Exiting... Thank you!");
+                    break;
+                default:
+                    System.out.println("Invalid option.");
+            }
+
+        } while (choice != 5);
+
+        sc.close();
+    }
+}
